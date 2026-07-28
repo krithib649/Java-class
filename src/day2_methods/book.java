ebf51e0be7d82fe735dbae1aa5d1cbe630facdb7
krithika <krithika@localhost>
2026-07-28 06:22:22 +0530

    books

---
+++ src/day2_methods/book.java
@@ -0,0 +1,31 @@
+package day2_methods;
+
+class book {
+	
+	String bookname;
+	String author;
+	int pages;
+	
+	book (String b, String a, int p){
+		
+		this.bookname = b;
+		this.author = a;
+		this.pages = p;
+	}
+	
+	void getData() {
+		
+		System.out.println(this.bookname + "   " + this.author + "   " + this.pages);
+		
+	}
+	
+	public static void main(String [] args) {
+		
+		book b1 = new book("Count of Monte Cristo","Alexandre Dumas",348);
+		book b2 = new book("The Hunger Games","Suzanne Collins",1000);
+		
+		b1.getData();
+		b2.getData();
+	}
+
+}

84f6cba4747891f2fcb4874a0a97763b043205d2
krithika <krithika@localhost>
2026-07-28 05:42:42 +0530

    test class

---
+++ src/day2_methods/Test.java
@@ -0,0 +1,24 @@
+package day2_methods;
+
+class Test {
+	
+	void girlsname()
+	{
+		System.out.println("Manvi");
+		System.out.println("Krithika");
+		System.out.println("rajashree");
+		System.out.println("aishwarya");
+		
+	}
+
+
+
+   public static void main(String[] args) {
+	   
+	   Test t1 = new Test();
+	   t1.girlsname();
+	   t1.girlsname();
+	   t1.girlsname();
+   }
+   
+}

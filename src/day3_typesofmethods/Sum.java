1e0a844367a94cad3da377afe768de2cffb52e6f
krithika <krithika@localhost>
2026-07-28 06:41:54 +0530

    sum of two numbers using methods

---
+++ src/day3_typesofmethods/Sum.java
@@ -0,0 +1,19 @@
+package day3_typesofmethods;
+
+class Sum {
+	
+	int add (int a, int b) {
+		
+		return a + b;
+	}
+	
+	public static void main(String [] args) {
+		
+		Sum s1 = new Sum();
+		s1.add(10,20);
+		System.out.println(s1.add(10,20));
+	}
+	
+	
+
+}

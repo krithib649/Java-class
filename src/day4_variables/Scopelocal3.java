7d173aaef420d8dd7167b749a93b5cb8ab564404
krithib649 <krithika.oct2008@gmail.com>
2026-07-30 13:13:35 +0530

    Fix case sensitivity and add comment on variable scope
    Corrected the casing of 'system' to 'System' in m1 method and added a comment in m2 method explaining the error.

--- src/day4_variables/Scopelocal3.java
+++ src/day4_variables/Scopelocal3.java
@@ -5,11 +5,11 @@ class Scopelocal3 {
 	void m1() {
 		
 		int a = 10;
-		system.out.println(a);
+		system.out.println(a); 
 	}
 	
 	void m2() {
 		
-		System.out.println(a);
+		System.out.println(a); //error as local variable cannot be accessed outside the member of the class
 	}
 }

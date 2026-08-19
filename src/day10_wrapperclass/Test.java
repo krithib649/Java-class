a59deb0d9aa93346f99083d961d1ed1dc9a9a6a4
krithika <krithika@localhost>
2026-08-20 00:11:34 +0530

    Auto-boxing example

---
+++ src/day10_wrapperclass/Test.java
@@ -0,0 +1,14 @@
+package day10_wrapperclass;
+
+class Test {
+	
+	public static void main(String [] args) {
+		
+		int i = 10;
+		Integer i1 = new Integer(i); //old version
+		Integer i2 = i1;
+		System.out.println(i1);
+		
+	}
+
+}

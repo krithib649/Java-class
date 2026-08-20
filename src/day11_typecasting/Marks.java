c4d3c6346cf4fb0c52284a89ee09a42f76105d22
krithika <krithika@localhost>
2026-08-20 09:26:27 +0530

    Implicit type casting example

---
+++ src/day11_typecasting/Marks.java
@@ -0,0 +1,11 @@
+package day11_typecasting;
+
+class Marks {
+	
+	public static void main(String [] args) {
+	int marks = 485;
+	double d = marks; //int automatically converts to double (implicit casting)
+	System.out.println(d);
+
+    }
+}

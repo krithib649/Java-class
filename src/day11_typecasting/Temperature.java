d820284ef6fe5dca136a28a00766ff24c0eb6382
krithika <krithika@localhost>
2026-08-20 09:27:27 +0530

    Explicit type casting example

---
+++ src/day11_typecasting/Temperature.java
@@ -0,0 +1,12 @@
+package day11_typecasting;
+
+class Temperature {
+	
+	public static void main(String [] args) {
+		float temperature  = 36.5f;
+	
+	int celsius = (int) temperature;
+	System.out.println(celsius);
+	}
+	
+}

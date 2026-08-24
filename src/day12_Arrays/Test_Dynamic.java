b6023611a424d86f6f140ba1edf263f43d17c46e
krithika <krithika@localhost>
2026-08-24 23:58:32 +0530

    Dynamic Arrays example

---
+++ src/day12_Arrays/Test_Dynamic.java
@@ -0,0 +1,28 @@
+package day12_Arrays;
+
+public class Test_Dynamic {
+	
+	public static void main(String [] args) {
+		
+		int[] arr = new int[5];
+		
+		arr[0] = 10;
+		arr[1] = 20;
+		arr[2] = 30;
+		arr[3] = 40;
+		arr[4] = 50;
+		
+		arr[0] = 100;
+		arr[1] = 250;
+		
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		System.out.println(arr[3]);
+		System.out.println(arr[4]);
+		
+		
+		
+		}
+
+}

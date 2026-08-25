9cbd4edd6599dc42a5a5af15b6a4a55b93c49c9c
krithika <krithika@localhost>
2026-08-25 11:50:08 +0530

    Static array (Declaration with initialization) direct example

---
+++ src/day12_Arrays/Exp_static.java
@@ -0,0 +1,17 @@
+package day12_Arrays;
+
+public class Exp_static {
+	
+	public static void main(String [] args) {
+		
+		String [] arr = {"Java","Python","SQL","C++"};
+		
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		System.out.println(arr[3]);
+		
+		
+	}
+
+}

c50190c0b9b7acfa23353042c5ea6eb4fd3a7199
krithika <krithika@localhost>
2026-08-26 12:57:55 +0530

    2d array declaration

---
+++ src/day12_Arrays/Declare2D.java
@@ -0,0 +1,14 @@
+package day12_Arrays;
+
+public class Declare2D {
+	
+	public static void main(String [] args) {
+		
+		int[][] arr;
+		arr = new int[3][4];
+		
+		System.out.println("Rows: " + arr.length);
+		System.out.println("Coulmns: " + arr[0].length);
+	}
+
+}

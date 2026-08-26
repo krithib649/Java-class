aea08d4f8116832471a3f2b8d460a7651de6116d
krithika <krithika@localhost>
2026-08-26 12:59:26 +0530

    Static 2d array direct declaration and initialization

---
+++ src/day12_Arrays/Static2D.java
@@ -0,0 +1,15 @@
+package day12_Arrays;
+
+public class Static2D {
+	
+	public static void main(String [] args ) {
+		
+		int[][] arr = {
+		 {1,2,3},
+		 {4,5,6},
+		 {6,7,8} };
+		
+		System.out.println(arr[0][2]);
+		System.out.println(arr[2][1]);
+	}
+}

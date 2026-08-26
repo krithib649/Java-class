9c38afee42854a49d2f3bd83ee35557fb659ad7e
krithika <krithika@localhost>
2026-08-26 12:22:59 +0530

    enhanced for loop (for each example)

---
+++ src/day12_Loops/ForeachExample.java
@@ -0,0 +1,16 @@
+package day12_Loops;
+
+public class ForeachExample {
+	
+	public static void main(String [] args) {
+		
+		int[] arr = {10,20,30,40,50};
+		
+		for (int val : arr) {   //enhanced for loop
+			
+			System.out.println(val);
+			
+		}
+	}
+
+}

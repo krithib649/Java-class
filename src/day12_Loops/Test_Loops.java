5cc3d03ca7a0599a9545fea31ee96165f9fa9cb6
krithika <krithika@localhost>
2026-08-26 12:23:47 +0530

    for loops on arrays example

---
+++ src/day12_Loops/Test_Loops.java
@@ -0,0 +1,15 @@
+package day12_Loops;
+
+public class Test_Loops {
+	
+	public static void main(String [] args) {
+		
+		String[] arr = {"Java","Python","C++"};
+		
+		for(int i = 0; i <= 2 ; i++) {
+			
+			System.out.println(arr[i]);
+		}
+	}
+
+}

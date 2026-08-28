f5671619734ed7839b05a73c5a9523b5373b7ddd
krithika <krithika@localhost>
2026-08-29 00:02:42 +0530

    Basic switch statement example

---
+++ src/day13_enumclass/Day1.java
@@ -0,0 +1,20 @@
+package day13_enumclass;
+
+enum Day1 { MONDAY, TUESDAY, WEDNESDAY }
+
+class EnumSwitch {
+    public static void main(String[] args) {
+        Day today = Day.TUESDAY;
+
+        switch (today) {
+            case MONDAY:
+                System.out.println("Start of week");
+                break;
+            case TUESDAY:
+                System.out.println("Second day");
+                break;
+            default:
+                System.out.println("Other day");
+        }
+    }
+}

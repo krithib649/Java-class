5d8e5a7d0d2d218ae8b6779b22410c9707bbd99b
krithika <krithika@localhost>
2026-08-30 21:21:54 +0530

    native keyword example

---
+++ src/day19_objectclassmethods/NativeExamp.java
@@ -0,0 +1,11 @@
+package day19_objectclassmethods;
+
+public class NativeExamp {
+	//no method body - just a declaration, ending with semicolon
+	public native void showMessage();
+	
+	public static void main(String [] args) {
+		
+	//calling a native method requires the library to be loaded first
+	}
+}

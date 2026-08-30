2c9c3589f6b1e718086b50a8b1d88a6fe0caf9d9
krithika <krithika@localhost>
2026-08-30 21:22:24 +0530

    hashcode method example

---
+++ src/day19_objectclassmethods/Test.java
@@ -0,0 +1,11 @@
+package day19_objectclassmethods;
+
+class Test extends Object {
+	
+	public static void main(String [] args) {
+		
+		Test t1 = new Test();
+		System.out.println(t1.hashCode()); //returns hashcode method
+		                                   // returns integer value
+	}
+}
